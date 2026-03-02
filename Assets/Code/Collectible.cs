e266ca1fb2c4f635818f7021091f7fcee122b7a7
Darja-Antonova <N00253685@iadt.ie>
2026-03-02 15:19:59 +0000

    AudioManager & Post Processing

--- Assets/Code/Collectible.cs
+++ Assets/Code/Collectible.cs
@@ -13,6 +13,7 @@ public class Collectible : MonoBehaviour
     {
         if(collision.CompareTag("Player"))
         {
+            AudioManager.Instance.PlaySFX(AudioManager.Instance.collectibleSFX);
             GameManager.Instance.AddCollectible();
             Destroy(gameObject);
         }
